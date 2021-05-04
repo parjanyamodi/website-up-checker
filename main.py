dbaa574a2f3b521881c6b65fd4342bd3ac3c2ed9
Parjanya Modi <71305818+parjanyamodi@users.noreply.github.com>
2021-05-04 19:47:30 +0530

    V 0.1.0

--- main.py
+++ main.py
@@ -1,31 +1,63 @@
 import mysql.connector
 import requests
-
+import threading
+import datetime
+import random
+import string
+global domains
+global column_name
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="toor",
-  database="website_db"
-)
+    host="localhost", user="root", password="toor", database="website_db")
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM websites_list")
+def createColumnname():
+    global column_name
+    column_name = [None] * 100
+    sample_string = 'pqrstuvwxy'
+    for i in range(100):
+        column_name[i] = ''.join((random.choice(sample_string)) for x in range(4))
 
-myresult = mycursor.fetchall()
+def getDomains():
+    global domains
+    mycursor.execute("SELECT domain_name FROM websites_list")
+    domains = mycursor.fetchall()
+    print(domains)
+    print(domains[0])
+    urls = [None] * len(domains)
+    for i in range(len(domains)):
+        tmp = str(domains[i])
+        sliceddomain = tmp[1:-2]
+        domains[i] = sliceddomain
+        slicedurl = tmp[2:-3]
+        urls[i] = slicedurl
+    return urls
 
-url = "activityportal.techalier.com/welcome.php"
 
-x = requests.get(url)
+def getStatus(urls, k):
+    res_code = [None] * len(urls)
+    for i in range(len(urls)):
+        print(i)
+        print(urls[i])
+        response = requests.get(urls[i])
+        res_code[i] = str(response.status_code)
 
-status_code = x.status_code
+    postStatus(res_code, k)
 
-sql = "INSERT INTO websites_list (domain_name, status_code) VALUES (%s, %s)"
-val = (url, x.status_code)
-mycursor.execute(sql, val)
 
-mydb.commit()
+def postStatus(res_code, k):
+    global domains
+    sql = "ALTER table websites_list ADD COLUMN {} VARCHAR(255)".format(column_name[k])
+    mycursor.execute(sql)
+    for i in range(len(domains)):
+        sql = "UPDATE websites_list SET {} = {};".format(column_name[k], res_code[i])
+        print(res_code[i])
+        mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    mydb.commit()
 
-print(x.status_code)
 
-for x in myresult:
-  print(x)
+if __name__ == "__main__":
+    createColumnname()
+    for i in range(100):
+        urls = getDomains()
+        getStatus(urls, i)
